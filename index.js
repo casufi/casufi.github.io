ab2695f9acb3c985a96528e1bf181d5dd7acd3d6
Volodymyr Kotulskyi <vladimirkotulskiy@gmail.com>
2016-02-17 13:52:55 +0200

    +

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 (function () {
     var placeholder = document.getElementsByClassName('placeholder');
+	if (placeholder) placeholder = placeholder[0];
+	if (placeholder && placeholder.style) console.log(typeof placeholder.style);
 	console.log(placeholder);
 	//.style.display='none';
  }());
