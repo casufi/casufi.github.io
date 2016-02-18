8a3feba51927f248e30736cf1dd910ae59669cbb
casufi <vladimirkotulskiy@gmail.com>
2016-02-18 19:24:24 +0000

    Read user data after Ctrl+F5

--- app/controllers/githubUserController.js
+++ app/controllers/githubUserController.js
@@ -1,7 +1,21 @@
 (function () {
     var githubUserController = function($scope, $routeParams, githubApiService){
+        
+        console.log("Hello world");
         var userName = $routeParams.userName;
-        $scope.githubuser = githubApiService.getUser(userName);
+        console.log(userName);
+        $scope.githubuser = githubApiService.getUserCashed(userName);
+        
+        if ((!$scope.githubuser||!$scope.githubuser.login) && userName){
+            githubApiService.getUser(userName)
+            .success(function(user){
+                $scope.githubuser = user;
+            })
+            .error(function(data, status, headers, config){
+                $scope.githubuser = {};
+            });            
+        }
+        
     };
     
     githubUserController.$inject = ['$scope','$routeParams', 'githubApiService'];
--- app/services/githabapi.js
+++ app/services/githabapi.js
@@ -10,12 +10,26 @@
             githubusers = users;
         }
         
+        this.getUserCashed = function(username){
+            if (typeof githubusers.filter === "function" && username){
+                var filteredusers = githubusers.filter(function(element){
+                    if (element && element.login && element.login === username) return 1
+                    else return 0;
+                });
+                console.log(filteredusers);
+                if (filteredusers && filteredusers[0]) return filteredusers[0]
+                else return {}
+            }else{
+                return {}
+            }
+        }
+        
         this.getUsers = function(){
           var users = $http.get('https://api.github.com/users');
           return users;  
         };
-        this.getUser = function(userName){
-          var user = $http.get('https://api.github.com/users/'+userName);
+        this.getUser = function(username){
+          var user = $http.get('https://api.github.com/users/'+username);
           return user;
         };
     };
